75351e4f28458683faddefbf79b4914ce8752e40
anuri01 <yang1234@gmail.com>
2025-08-05 23:38:01 +0900

    새 글 작성 페이지 및 스타일 추가, 게시물 등록 기능 구현

---
+++ client/src/pages/WritePage.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import api from "../api/axiosConfig";
+import { Link, useNavigate } from "react-router-dom";
+import useUserStore from "../store/userStore";
+import './WritePage.css';
+
+function WritePage() {
+
+const navigate = useNavigate();
+const { isLoggedIn } = useUserStore();
+const [ title, setTitle ] = useState('');
+const [ content, setContent ] = useState('');
+
+// 새글 작성 폼 제출 시 실행될 함수
+const handlePostSubmit = async (e) => {
+    // form 제출시 기본 동작 막음
+    e.preventDefault();
+    if (!title || !content) {
+        alert('제목과 내용을 모두 입력해 주세요.');
+        return;
+    }
+    
+    try {
+        const response = await api.post('/posts', { title, content });
+        // 게시물 등록 성공 시 입력폼 초기화 
+        setTitle('');
+        setContent('');
+        navigate(`/post/${response.data._id}`);
+
+    } catch(error) {
+        console.log('게시글 작성에 실패했어요.', error);
+        alert('게시글 작성에 실패했어요.');
+    }
+};
+
+return (
+        <div className="wirte-page">
+            <h3>새 글 작성하기</h3>
+            { isLoggedIn && (
+                <section className="post-creator">
+                    <h3>새 글 작성하기</h3>
+                    <form onSubmit={handlePostSubmit}>
+                        <input 
+                        className="form-input"
+                        type="text"
+                        placeholder="게시물 제목을 입력하세요."
+                        value={title}
+                        maxLength={50}
+                        onChange={(e) => setTitle(e.target.value)}
+                        />
+                        <textarea 
+                        className="form-textarea"
+                        placeholder="내용을 입력하세요."
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        />
+                        <button type="submit" className="button button-primary">등록</button>
+                        <Link to={'/'}>
+                        <button type="button" className="button">취소</button>
+                        </Link>
+                    </form>
+                </section>
+            )}
+            </div>
+)
+}
+
+export default WritePage;
